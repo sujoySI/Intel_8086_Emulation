0726bb8729330b3e978e1f37df297c52373bca67
Sujoy Sinha <900sujoysinha@gmail.com>
2025-09-25 18:13:40 +0530

    Small Changes to names of Variables.
    Small Changes to names of Variables. Some changes to .gitignore.

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,54 @@
 #include <iostream>
-#include <bitset>                                           //Might or might not use it
-#include <cstring>                                          //memset()
+#include <bitset>                                          //Might or might not use it
+#include <cstring>                                         //memset()
 #include <cstdint>
 
 using namespace std;
 
-using Byte = uint8_t;                                       // For ease of use
-using Word = uint16_t;                                      // For ease of use
-using Sentence = uint32_t;                                  // For ease of use
+using UI8 = uint8_t;                                       // For ease of use
+using UI16 = uint16_t;                                     // For ease of use
+using UI32 = uint32_t;                                     // For ease of use
 
 //General Purpose Registers
 union GPR
 {
-    Word X;                                                 //X for AX, BX, CX and DX
+    UI16 X;                                                //X for AX, BX, CX and DX
     struct
     {
-        Byte H;                                             //High for AH, BH, CH, DH
-        Byte L;                                             //Low  for AL, BL, CL, DL
+        UI8 H;                                             //High for AH, BH, CH, DH
+        UI8 L;                                             //Low  for AL, BL, CL, DL
     };
 };
 
 //Status Register (Flags Are bits)
 union FLAG{
-    Word Flags;
+    UI16 Flags;
     struct {
-        Word CF : 1;                                        //Carry Flag
-        Word    : 1;                                        //Reserved
-        Word PF : 1;                                        //Parity Flag
-        Word    : 1;                                        //Reserved
-        Word AF : 1;                                        //Auxiliary Carry Flag
-        Word    : 1;                                        //Reserved
-        Word ZF : 1;                                        //Zero Flag
-        Word SF : 1;                                        //Sign Flag
-        Word TF : 1;                                        //Trap Flag
-        Word IF : 1;                                        //Interrupt Flag
-        Word DF : 1;                                        //Direction Flag
-        Word OF : 1;                                        //Overflow Flag
-        Word    : 4;                                        //Reserved
+        UI16 CF : 1;                                        //Carry Flag
+        UI16    : 1;                                        //Reserved
+        UI16 PF : 1;                                        //Parity Flag
+        UI16    : 1;                                        //Reserved
+        UI16 AF : 1;                                        //Auxiliary Carry Flag
+        UI16    : 1;                                        //Reserved
+        UI16 ZF : 1;                                        //Zero Flag
+        UI16 SF : 1;                                        //Sign Flag
+        UI16 TF : 1;                                        //Trap Flag
+        UI16 IF : 1;                                        //Interrupt Flag
+        UI16 DF : 1;                                        //Direction Flag
+        UI16 OF : 1;                                        //Overflow Flag
+        UI16    : 4;                                        //Reserved
     };
 };
 
 struct Memory
 {
-    static constexpr Sentence MAX_MEM = 1024 * 1024;        //Max Memory of 8086
-    Sentence AMA;                                           //Absolute Memory Address (Calculated from IP and CS)
-    Byte Data[MAX_MEM];
+    static constexpr UI32 MAX_MEM = 1024 * 1024;            //Max Memory of 1MB for 8086
+    UI32 AMA;                                               //Absolute Memory Address (Calculated from IP and CS)
+    UI8 Data[MAX_MEM];
 
     void INITIALISE()
     {
-        memset(Data, 0x00, MAX_MEM * sizeof(Data[0]));  // Sets all byte element to 0x00
+        memset(Data, 0x00, MAX_MEM * sizeof(Data[0]));  //Sets all byte element to 0x00
     }
 };
 
@@ -61,38 +61,46 @@ struct CPU
     GPR D;                                                  //Data Register
 
     //Index Registers
-    Word SI;                                                //Source Index
-    Word DI;                                                //Destination Index
-    Word BP;                                                //Base Pointer
-    Word SP;                                                //Stack Pointer
+    UI16 SI;                                                //Source Index
+    UI16 DI;                                                //Destination Index
+    UI16 BP;                                                //Base Pointer
+    UI16 SP;                                                //Stack Pointer
 
     //Program Counter
-    Word IP;                                                //Instruction Pointer
+    UI16 IP;                                                //Instruction Pointer
 
     //Segment Registers
-    Word CS;                                                //Code Segment
-    Word DS;                                                //Data Segment
-    Word ES;                                                //Extra Segment
-    Word SS;                                                //Stack Segment
+    UI16 CS;                                                //Code Segment
+    UI16 DS;                                                //Data Segment
+    UI16 ES;                                                //Extra Segment
+    UI16 SS;                                                //Stack Segment
 
     //Status Flag Register
     FLAG FR;
+
+    //Prefetch Queue for Instructions
+    UI8 IQ[6];
 
     void RESET( Memory& memory )
     {
         IP = 0x0000;
         CS = 0xffff;
         DS = ES = SS = 0x0000;
-        FR.Flags = 0;
+        memset(IQ, 0x00, 6 * sizeof(IQ[0]));            //Clears Instruction Prefetch Queue
+        FR.Flags = 0x00000000;
         if (IP == 0x0000 && CS == 0xffff) {
             memory.AMA = (CS * 0x10) + IP;
         }
         memory.INITIALISE();
     }
 
-    void EXECUTE( Sentence Ticks, Memory& memory )
+    void STARUP() {
+
+    }
+
+    void EXECUTE( UI32 Cycles, Memory& memory )
     {
-        cout<<"\nTicks: "<<Ticks<<"\n";
+        cout<<"\nCycles: "<<Cycles<<"\n";
     }
 };
 
